1d3df3cff4e63507d54c79d511d7b12aa1560562
Seper Kristof <48735954+sepkri@users.noreply.github.com>
2022-11-17 12:20:13 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
+
+
 (function($) {
 
 	// Settings.
@@ -153,6 +157,14 @@
 				});
 
 			}
+	
+	
+	
+
+	
+	
+	
+	
 
 	// Keyboard shortcuts.
 		if (settings.keyboardShortcuts.enabled)
